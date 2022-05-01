a12fbdb3ccd3bb233740d1a456d7b3791b68f45d
wiktor <wikcioo@gmail.com>
2022-05-02 00:38:50 +0200

    Added user DAO interface and implmemented it with SQL

---
+++ RentalSystem/src/application/dao/UserDao.java
@@ -0,0 +1,11 @@
+package application.dao;
+
+import application.model.User;
+
+import java.sql.SQLException;
+
+public interface UserDao {
+    User create(String firstName, String lastName, String password) throws SQLException;
+    User get(String firstName, String lastName) throws SQLException;
+    void update(User user) throws SQLException;
+}

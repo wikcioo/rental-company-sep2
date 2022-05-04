96f2c6202c3da9efd7c95e3bed4865e6bbd06f11
wiktor <wikcioo@gmail.com>
2022-05-04 11:32:20 +0200

    Added client instantiating and pass it to the model

--- RentalSystem/src/application/RentalSystemApplication.java
+++ RentalSystem/src/application/RentalSystemApplication.java
@@ -1,5 +1,7 @@
 package application;
 
+import application.client.RentalSystemClient;
+import application.client.RentalSystemClientImplementation;
 import application.model.Model;
 import application.model.ModelManager;
 import application.view.ViewHandler;
@@ -7,10 +9,13 @@ import application.viewmodel.ViewModelFactory;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.rmi.registry.Registry;
+
 public class RentalSystemApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Model model = new ModelManager();
+        RentalSystemClient client = new RentalSystemClientImplementation("localhost", Registry.REGISTRY_PORT);
+        Model model = new ModelManager(client);
         ViewModelFactory viewModelFactory = new ViewModelFactory(model);
         ViewHandler viewHandler = new ViewHandler(viewModelFactory);
         viewHandler.start(primaryStage);

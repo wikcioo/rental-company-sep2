22805870d9dccd3590f24a7838fb3f48e7e99d89
wiktor <wikcioo@gmail.com>
2022-05-04 10:38:36 +0200

    Add DAO fields to the server

--- RentalSystem/src/application/server/RentalSystemServer.java
+++ RentalSystem/src/application/server/RentalSystemServer.java
@@ -1,25 +1,41 @@
 package application.server;
 
+import application.dao.EquipmentDao;
+import application.dao.SQLEquipmentDao;
+import application.dao.SQLUserDao;
+import application.dao.UserDao;
 import application.model.Equipment;
 import application.shared.IServer;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class RentalSystemServer implements IServer {
+    private final EquipmentDao equipmentDao;
+    private final UserDao userDao;
     public RentalSystemServer() throws RemoteException {
+        this.equipmentDao = SQLEquipmentDao.getInstance();
+        this.userDao = SQLUserDao.getInstance();
         UnicastRemoteObject.exportObject(this, 0);
     }
 
     @Override
     public void addEquipment(Equipment equipment) throws RemoteException {
-        // TODO: Complete the method once we have DAO implemented
+        try {
+            equipmentDao.add(equipment);
+        } catch (SQLException e) {
+            throw new RemoteException(e.getMessage(), e);
+        }
     }
 
     @Override
     public ArrayList<Equipment> getAllEquipment() throws RemoteException {
-        // TODO: Complete the method once we have DAO implemented
-        return null;
+        try {
+            return equipmentDao.getAll();
+        } catch (SQLException e) {
+            throw new RemoteException(e.getMessage(), e);
+        }
     }
 }

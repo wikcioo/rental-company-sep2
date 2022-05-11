cb3ad493931598a60614f14dea225ae51764f901
wiktor <wikcioo@gmail.com>
2022-05-11 03:05:29 +0200

    Added methods that call respective DAO

--- RentalSystem/src/application/server/RentalSystemServer.java
+++ RentalSystem/src/application/server/RentalSystemServer.java
@@ -38,4 +38,22 @@ public class RentalSystemServer implements IServer {
             throw new RemoteException(e.getMessage(), e);
         }
     }
+
+    @Override
+    public boolean isValidUser(String email, String password) throws RemoteException {
+        try {
+            return userDao.isValidUser(email, password);
+        } catch (SQLException e) {
+            throw new RemoteException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public boolean isUserAManager(String email) throws RemoteException {
+        try {
+            return userDao.isUserAManager(email);
+        } catch (SQLException e) {
+            throw new RemoteException(e.getMessage(), e);
+        }
+    }
 }
--- RentalSystem/src/application/shared/IServer.java
+++ RentalSystem/src/application/shared/IServer.java
@@ -4,9 +4,12 @@ import application.model.Equipment;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface IServer extends Remote {
     void addEquipment(Equipment equipment) throws RemoteException;
     ArrayList<Equipment> getAllEquipment() throws RemoteException;
+    boolean isValidUser(String email, String password) throws RemoteException;
+    boolean isUserAManager(String email) throws RemoteException;
 }

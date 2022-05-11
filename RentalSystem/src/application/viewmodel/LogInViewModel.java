48505f2b7707a00f128f3abd2d989569bbe1cbec
wiktor <wikcioo@gmail.com>
2022-05-11 03:08:32 +0200

    Changed StringProperty from name to email

--- RentalSystem/src/application/viewmodel/LogInViewModel.java
+++ RentalSystem/src/application/viewmodel/LogInViewModel.java
@@ -7,27 +7,27 @@ import javafx.beans.property.StringProperty;
 import java.rmi.RemoteException;
 
 public class LogInViewModel {
-    private final StringProperty name;
+    private final StringProperty email;
     private final StringProperty password;
     private final Model model;
 
     public LogInViewModel(Model model) {
         this.model = model;
-        name = new SimpleStringProperty("");
+        email = new SimpleStringProperty("");
         password = new SimpleStringProperty("");
     }
 
-    public void bindName(StringProperty property) {
-        name.bind(property);
+    public void bindEmail(StringProperty property) {
+        email.bind(property);
     }
 
     public void bindPassword(StringProperty property) {
         password.bind(property);
     }
 
-    public boolean logIn(){
+    public String logIn(){
         try {
-            return model.logIn(name.get(), password.get());
+            return model.logIn(email.get(), password.get());
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         }

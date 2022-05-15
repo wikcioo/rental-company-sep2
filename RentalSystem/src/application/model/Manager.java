fec1b9b5463b6afe281846cdc8614fa9c995dcab
wiktor <wikcioo@gmail.com>
2022-05-15 22:26:07 +0200

    Added new type of user

---
+++ RentalSystem/src/application/model/Manager.java
@@ -0,0 +1,7 @@
+package application.model;
+
+public class Manager extends User {
+    public Manager(String firstName, String lastName, String phoneNumber, String email, String password) {
+        super(firstName, lastName, phoneNumber, email, password);
+    }
+}
---
+++ RentalSystem/src/application/model/Rentee.java
@@ -0,0 +1,7 @@
+package application.model;
+
+public class Rentee extends User {
+    public Rentee(String firstName, String lastName, String phoneNumber, String email, String password) {
+        super(firstName, lastName, phoneNumber, email, password);
+    }
+}

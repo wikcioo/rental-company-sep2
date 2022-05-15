0267f5564aaa26ebc1cd7ad6e950a1f559bb7e9f
wiktor <wikcioo@gmail.com>
2022-05-15 22:32:54 +0200

    Added phone number and email fields. Added getters and setters and toString. Changed constructor to accept all required fields

--- RentalSystem/src/application/model/User.java
+++ RentalSystem/src/application/model/User.java
@@ -5,36 +5,66 @@ import java.io.Serializable;
 public class User implements Serializable {
     private String firstName;
     private String lastName;
+    private String phoneNumber;
+    private String email;
     private String password;
 
-    public User(String firstName, String lastName) {
-        this(firstName, lastName, null);
-    }
-
-    public User(String firstName, String lastName, String password) {
+    public User(String firstName, String lastName, String phoneNumber, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
         this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Rentee: " + firstName + " " + lastName;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public String getFullName() {
-        return firstName + " " + lastName;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }

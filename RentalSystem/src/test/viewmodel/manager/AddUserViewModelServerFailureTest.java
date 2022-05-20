d0390517079e870538b617fab47a81ab852bb967
wiktor <wikcioo@gmail.com>
2022-05-20 15:47:43 +0200

    Added unit testing for AddUserViewModel

---
+++ RentalSystem/src/test/viewmodel/manager/AddUserViewModelServerFailureTest.java
@@ -0,0 +1,33 @@
+package test.viewmodel.manager;
+
+import application.model.Model;
+import application.model.ModelManager;
+import application.viewmodel.manager.AddUserViewModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import test.client.FailingRentalSystemClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class AddUserViewModelServerFailureTest {
+    private AddUserViewModel viewModel;
+    private StringProperty error;
+
+    @BeforeEach
+    public void setUp() {
+        Model model = new ModelManager(new FailingRentalSystemClient());
+        viewModel = new AddUserViewModel(model);
+        error = new SimpleStringProperty();
+        viewModel.bindError(error);
+    }
+
+    @Test
+    public void server_failure_during_adding_new_user() {
+        boolean result = viewModel.addUser("Manager");
+        assertEquals("Failed to add user to the database", error.get());
+        assertFalse(result);
+    }
+}

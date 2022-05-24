c641b8502da73009e33215ff2aba17df7bf185d7
M0Rek <57091441+M0Rek@users.noreply.github.com>
2022-05-24 15:25:53 +0200

    Unreserved equipment query redone
    Why did it take so long I dont know

--- RentalSystem/src/application/dao/SQLEquipmentDao.java
+++ RentalSystem/src/application/dao/SQLEquipmentDao.java
@@ -72,38 +72,28 @@ public class SQLEquipmentDao implements EquipmentDao {
                 Statement statement = connection.createStatement()
         ) {
             ResultSet rs = statement.executeQuery("\n" +
-                    "SELECT * FROM equipment WHERE equipment_id\n" +
-                    "                                  NOT IN (SELECT reservation.equipment_id FROM reservation)\n" +
-                    "                                OR (\n" +
-                    "                                    equipment_id IN (SELECT reservation.equipment_id FROM reservation)\n" +
-                    "                                        AND equipment_id NOT IN (\n" +
-                    "                                            SELECT equipment_id FROM reservation\n" +
-                    "                                                LEFT JOIN approved a on reservation.reservation_id = a.reservation_id\n" +
-                    "                                                LEFT JOIN rejected r on reservation.reservation_id = r.reservation_id\n" +
-                    "                                                LEFT JOIN expired e on reservation.reservation_id = e.reservation_id\n" +
-                    "                                            WHERE a.reservation_id IS NULL\n" +
-                    "                                            AND r.reservation_id IS NULL\n" +
-                    "                                            AND e.reservation_id IS NULL\n" +
-                    "                                            )\n" +
-                    "                                        AND equipment_id NOT IN (\n" +
-                    "                                            SELECT equipment_id FROM reservation\n" +
-                    "                                                INNER JOIN (\n" +
-                    "                                                approved LEFT JOIN returned r2 on approved.reservation_id = r2.approved_id\n" +
-                    "                                                )\n" +
-                    "                                                        on reservation.reservation_id = approved.reservation_id\n" +
-                    "                                            )\n" +
-                    "            );\n");
+                    "\n" +
+                    "SELECT equipment.equipment_id as id, model, category, availability FROM equipment\n" +
+                    "    -- joining equipment that's not returned, rejected or expired or never reserved before\n" +
+                    "    LEFT JOIN (\n" +
+                    "        SELECT equipment_id FROM reservation\n" +
+                    "            LEFT JOIN ( approved INNER JOIN returned r2 on approved.reservation_id = r2.approved_id )\n" +
+                    "                as returned on returned.reservation_id = reservation.reservation_id\n" +
+                    "            LEFT JOIN rejected r on reservation.reservation_id = r.reservation_id\n" +
+                    "            LEFT JOIN expired e on reservation.reservation_id = e.reservation_id\n" +
+                    "        WHERE r.reservation_id IS NULL\n" +
+                    "          AND returned.reservation_id IS NULL\n" +
+                    "          AND e.reservation_id IS NULL) as currently_reserved on equipment.equipment_id = currently_reserved.equipment_id\n" +
+                    "    -- and selecting equipment that does not match joined criteria\n" +
+                    "    WHERE currently_reserved.equipment_id IS NULL\n" +
+                    "ORDER BY id;");
             while (rs.next()) {
-                int id = rs.getInt("equipment_id");
+                int id = rs.getInt("id");
                 String model = rs.getString("model");
                 String category = rs.getString("category");
                 boolean available = rs.getBoolean("availability");
                 equipmentList.add(new Equipment(id, model, category, available));
             }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-            throw new SQLException();
         }
 
         return equipmentList;

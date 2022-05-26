e4da2572a1827352c6b15ff2e797dd0c7bc92f37
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2022-05-26 13:24:27 +0200

    Add toString methods to reservations

--- RentalSystem/src/application/model/reservations/Expired.java
+++ RentalSystem/src/application/model/reservations/Expired.java
@@ -4,6 +4,7 @@ import application.model.users.User;
 import application.model.equipment.Equipment;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Expired extends Reservation {
     private final LocalDateTime dateTime;
@@ -26,5 +27,11 @@ public class Expired extends Reservation {
 
     public LocalDateTime getExpirationDate() {
         return dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nExpired on: " + dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }

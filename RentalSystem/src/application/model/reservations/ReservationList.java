a7e29250afae15133c333b73fffc3d4dead0a84b
M0Rek <57091441+M0Rek@users.noreply.github.com>
2022-05-26 16:30:11 +0200

    Statemachine diagram and added universal method for getting reservations

--- RentalSystem/src/application/model/reservations/ReservationList.java
+++ RentalSystem/src/application/model/reservations/ReservationList.java
@@ -13,6 +13,18 @@ public class ReservationList {
         return reservationList;
     }
 
+
+
+    public ArrayList<Reservation> getReservationsByType(String type, ArrayList<T> e) {
+        ArrayList<Reservation> result = new ArrayList<>();
+        for (Reservation reservation : reservations) {
+            if (reservation.status().equals(type)) {
+                result.add(reservation);
+            }
+        }
+        return result;
+    }
+
     public ArrayList<Reservation> getUnapprovedReservations() {
         ArrayList<Reservation> result = new ArrayList<>();
         for (Reservation reservation : reservations) {

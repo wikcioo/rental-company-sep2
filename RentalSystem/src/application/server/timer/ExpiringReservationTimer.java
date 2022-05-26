e8621d01825efed2184a2372281a3b81b56b87ed
M0Rek <57091441+M0Rek@users.noreply.github.com>
2022-05-27 00:38:09 +0200

    Implemented reservation expiration
    By default, all reservations will expire after 1800 seconds

--- RentalSystem/src/application/server/timer/ExpiringReservationTimer.java
+++ RentalSystem/src/application/server/timer/ExpiringReservationTimer.java
@@ -4,6 +4,7 @@ import application.model.reservations.Reservation;
 import application.util.NamedPropertyChangeSubject;
 
 public interface ExpiringReservationTimer extends NamedPropertyChangeSubject {
+    String RESERVATION_EXPIRED = "reservation_expired";
     void addReservationToExpire(Reservation reservation);
     void cancelExpiration(int id);
     void cancelExpiration(Reservation reservation);

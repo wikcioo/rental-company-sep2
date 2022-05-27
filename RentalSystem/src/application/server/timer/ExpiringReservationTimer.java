a827ee85e4ff3c004b72b0d4d961aa3d83483440
M0Rek <57091441+M0Rek@users.noreply.github.com>
2022-05-27 08:19:10 +0200

    Added javadoc for timers

--- RentalSystem/src/application/server/timer/ExpiringReservationTimer.java
+++ RentalSystem/src/application/server/timer/ExpiringReservationTimer.java
@@ -5,9 +5,34 @@ import application.util.NamedPropertyChangeSubject;
 
 public interface ExpiringReservationTimer extends NamedPropertyChangeSubject {
     String RESERVATION_EXPIRED = "reservation_expired";
-    void addReservationToExpire(Reservation reservation);
-    void cancelExpiration(int id);
-    void cancelExpiration(Reservation reservation);
+
+    /**Adds a reservation that will expire after amount of time given in the constructor
+     *
+     * @param reservation
+     * @throws IllegalArgumentException if given reservation is not unapproved
+     */
+    void addReservationToExpire(Reservation reservation) throws IllegalArgumentException;
+
+    /**Cancels the expiration of the reservation with given id
+     * @param id id of the reservation
+     * @throws IllegalArgumentException if there is no reservation that is currently set to expire with given id
+     */
+    void cancelExpiration(int id) throws IllegalArgumentException;
+
+    /**Cancels the expiration of the reservation
+     * @param reservation reservation that is set to expire
+     * @throws IllegalArgumentException if given reservation is not currently set to expire
+     */
+    void cancelExpiration(Reservation reservation) throws IllegalArgumentException;
+
+    /**Checks whether the given reservation is currently set to expire
+     * @param reservation
+     * @return true if given reservation is set to expire, otherwise false
+     */
     boolean isExpiring(Reservation reservation);
+
+    /**Cancels the entire timer  and creates a new one subsequently canceling all scheduled expirations
+     *
+     */
     void cancelAll();
 }
--- RentalSystem/src/application/server/timer/SelectiveReservationTimer.java
+++ RentalSystem/src/application/server/timer/SelectiveReservationTimer.java
@@ -5,5 +5,9 @@ import application.model.reservations.Reservation;
 import java.util.ArrayList;
 
 public interface SelectiveReservationTimer extends ExpiringReservationTimer{
+
+    /**Cancels all current timers and iterates through the arraylist. Adds all unapproved reservations, the rest is ignored
+     * @param reservations reservations with different current statuses
+     */
     void setAllUnapprovedReservations(ArrayList<Reservation> reservations);
 }

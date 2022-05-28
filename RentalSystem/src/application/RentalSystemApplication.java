28bccfcf7714d317e61b707346c376a66228ea22
wiktor <wikcioo@gmail.com>
2022-05-28 13:56:59 +0200

    Passing model manager 3 times to viewmodelfactory

--- RentalSystem/src/application/RentalSystemApplication.java
+++ RentalSystem/src/application/RentalSystemApplication.java
@@ -2,8 +2,7 @@ package application;
 
 import application.client.RentalSystemClient;
 import application.client.RentalSystemClientImplementation;
-import application.model.Model;
-import application.model.ModelManager;
+import application.model.models.ModelManager;
 import application.view.ViewHandler;
 import application.viewmodel.ViewModelFactory;
 import javafx.application.Application;
@@ -15,8 +14,8 @@ public class RentalSystemApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         RentalSystemClient client = new RentalSystemClientImplementation("localhost", Registry.REGISTRY_PORT);
-        Model model = new ModelManager(client);
-        ViewModelFactory viewModelFactory = new ViewModelFactory(model);
+        ModelManager modelManager = new ModelManager(client);
+        ViewModelFactory viewModelFactory = new ViewModelFactory(modelManager, modelManager, modelManager);
         ViewHandler viewHandler = new ViewHandler(viewModelFactory);
         viewHandler.start(primaryStage);
     }
